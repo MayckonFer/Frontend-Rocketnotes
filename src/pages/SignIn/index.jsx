3603fceaac92be8e1fb0e2908ce8250fc2f63103
Mayckon Fernandes <mayckonfernandes1@gmail.com>
2023-01-19 11:09:30 -0300

    consumo da api...

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Container, Form, Background } from "./styles";
 
 import { Link } from "react-router-dom";
@@ -7,7 +8,18 @@ import { Button } from "../../components/Button";
 
 import { FiMail, FiLock } from "react-icons/fi";
 
+import { useAuth } from "../../hooks/auth";
+
 export function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { signIn } = useAuth();
+
+  function handleSignIn() {
+    signIn({ email, password });
+  }
+
   return (
     <Container>
       <Form>
@@ -15,10 +27,20 @@ export function SignIn() {
         <p>Aplicação para salvar e gerenciar seus links úteis</p>
         <h2>Faça seu login</h2>
 
-        <Input placeholder="E-mail" type="text" icon={FiMail} />
-        <Input placeholder="Senha" type="password" icon={FiLock} />
+        <Input
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="E-mail"
+          type="text"
+          icon={FiMail}
+        />
+        <Input
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Senha"
+          type="password"
+          icon={FiLock}
+        />
 
-        <Button title="Entrar" />
+        <Button title="Entrar" type="button" onClick={handleSignIn} />
         <Link to="/register">Criar conta</Link>
       </Form>
 
--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import { Container, Form, Background } from "./styles";
 
-import { Link } from "react-router-dom";
+import { api } from "../../services/api";
+
+import { Link, useNavigate } from "react-router-dom";
 
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
@@ -8,6 +11,33 @@ import { Button } from "../../components/Button";
 import { FiMail, FiLock, FiUser } from "react-icons/fi";
 
 export function SignUp() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  function handleSignUp(e) {
+    if (!name || !email || !password) {
+      alert("Preencha todos os campos");
+      return;
+    }
+
+    api
+      .post("/users", { name, email, password })
+      .then(() => {
+        alert("Usuário cadastrado com sucesso!");
+        navigate("/");
+      })
+      .catch((error) => {
+        if (error.response) {
+          alert(error.response.data.message);
+        } else {
+          alert("Não foi possível cadastrar");
+        }
+      });
+  }
+
   return (
     <Container>
       <Background />
@@ -16,11 +46,26 @@ export function SignUp() {
         <p>Aplicação para salvar e gerenciar seus links úteis</p>
         <h2>Crie sua conta</h2>
 
-        <Input placeholder="Nome" type="text" icon={FiUser} />
-        <Input placeholder="E-mail" type="text" icon={FiMail} />
-        <Input placeholder="Senha" type="password" icon={FiLock} />
+        <Input
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Nome"
+          type="text"
+          icon={FiUser}
+        />
+        <Input
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="E-mail"
+          type="text"
+          icon={FiMail}
+        />
+        <Input
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Senha"
+          type="password"
+          icon={FiLock}
+        />
 
-        <Button title="Cadastrar" />
+        <Button title="Cadastrar" type="button" onClick={handleSignUp} />
         <Link to="/">Voltar para o login</Link>
       </Form>
     </Container>
