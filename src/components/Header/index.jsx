1bddf97c8e48dcc4afb3510bb20dad4b131fce94
Mayckon Fernandes <mayckonfernandes1@gmail.com>
2023-01-24 08:12:23 -0300

    profile

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,20 +1,30 @@
 import { RiShutDownLine } from "react-icons/ri";
 import { Container, Profile, Logout, Content } from "./styles";
 
+import { useAuth } from "../../hooks/auth";
+import { api } from "../../services/api";
+import avatarPlaceholder from "../../assets/avatar_placeholder.svg";
+
 export function Header() {
+  const { signOut, user } = useAuth();
+
+  const avatarUrl = user.avatar
+    ? `${api.defaults.baseURL}/files/${user.avatar}`
+    : avatarPlaceholder;
+
   return (
     <Container>
       <Content>
         <Profile to="/profile">
-          <img src="https://github.com/MayckonFer.png" alt="Foto do usuário" />
+          <img src={avatarUrl} alt={user.name} />
 
           <div>
             <span>Bem-vindo</span>
-            <strong>Mayckon Fernandes</strong>
+            <strong>{user.name}</strong>
           </div>
         </Profile>
 
-        <Logout>
+        <Logout onClick={signOut}>
           <RiShutDownLine />
         </Logout>
       </Content>
--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -14,7 +14,7 @@ function AuthProvider({ children }) {
       localStorage.setItem("@rocketnotes:user", JSON.stringify(user));
       localStorage.setItem("@rocketnotes:token", token);
 
-      api.defaults.headers.authorization = `Bearer ${token}`;
+      api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
       setData({ user, token });
     } catch (error) {
       if (error.response) {
@@ -25,12 +25,43 @@ function AuthProvider({ children }) {
     }
   }
 
+  function signOut() {
+    localStorage.removeItem("@rocketnotes:token");
+    localStorage.removeItem("@rocketnotes:user");
+
+    setData({});
+  }
+
+  async function updateProfile({ user, avatarFile }) {
+    try {
+      if (avatarFile) {
+        const fileUploadForm = new FormData();
+        fileUploadForm.append("avatar", avatarFile);
+
+        const response = await api.patch("/users/avatar", fileUploadForm);
+        user.avatar = response.data.avatar;
+      }
+
+      await api.put("/users", user);
+      localStorage.setItem("rocketnotes:user", JSON.stringify(user));
+
+      setData({ user, token: data.token });
+      alert("Perfil atualizado!");
+    } catch (error) {
+      if (error.response) {
+        alert(error.response.data.message);
+      } else {
+        alert("Não foi possível atualizar o perfil.");
+      }
+    }
+  }
+
   useEffect(() => {
     const token = localStorage.getItem("@rocketnotes:token");
     const user = localStorage.getItem("@rocketnotes:user");
 
     if (token && user) {
-      api.defaults.headers.authorization = `Bearer ${token}`;
+      api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 
       setData({
         token,
@@ -41,7 +72,9 @@ function AuthProvider({ children }) {
 
   return (
     <>
-      <AuthContext.Provider value={{ signIn, user: data.user }}>
+      <AuthContext.Provider
+        value={{ signIn, signOut, updateProfile, user: data.user }}
+      >
         {children}
       </AuthContext.Provider>
     </>
